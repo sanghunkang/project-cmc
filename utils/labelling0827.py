4fdcf0dcf900eabac925745a4037960c8e137f84
sanghunKang <sanghunkang.dev@gmail.com>
2017-08-27 16:04:22 +0900

    image sort

---
+++ utils/labelling0827.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Import built-in packages
+import argparse, os, sys, shutil
+
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+# parser.add_argument("echo", help="Example: python labelling0827.py -s /dir/of/images -d dir/of/images-sorted -f /path/to/csv")
+parser.add_argument("-s", "--src", type=str, help="Directory where original data are stored.")
+parser.add_argument("-d", "--dst", type=str, help="Directory where original data will be copied into its subdirectories according to their classfications.")
+parser.add_argument("-r", "--ref", type=str, help="path to .csv file where relevant data is located.")
+args = parser.parse_args()
+
+dir_src = "./"
+dir_dst = "./"
+fpath_ref = ""
+
+if args.src: dir_src = args.src
+if args.dst: dir_dst = args.dst
+if args.reference: fpath_ref = args.reference
+
+# Check on which system the programme is running
+print(sys.platform)
+
+df = pd.read_csv(fpath_ref)
+
+def check_dir(fpath):
+    if not os.path.isdir(fpath.rsplit("/", 1)[0]): os.mkdir(fpath.rsplit("/", 1)[0])
+
+arr_fpath_src = [os.path.join(dir_src, str(df["PID"][i]) + ".bmp") for i in range(df.shape[0])]
+arr_fpath_dst = [os.path.join(dir_dst, "{}/{}.bmp".format(df["TYPE1"][i][0], df["PID"][i])) for i in range(df.shape[0])]
+
+for i in range (df.shape[0]):
+	try:
+		check_dir(arr_fpath_dst[i])
+		shutil.copy(arr_fpath_src[i], arr_fpath_dst[i])
+	except FileNotFoundError:
+		print("Not if the reference file")
