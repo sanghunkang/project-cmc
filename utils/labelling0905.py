611789f0c66b30ea89d846ec64248e04e8a815bd
sanghunkang <sanghunkang.dev@gmail.com>
2017-09-05 15:15:18 +0900

    file classifier

---
+++ utils/labelling0905.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Import built-in packages
+import argparse, os, sys, shutil
+
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--src", type=str, help="Directory where original data are stored.")
+parser.add_argument("-d", "--dst", type=str, help="Directory where original data will be copied into its subdirectories according to their classfications.")
+parser.add_argument("-r", "--ref", type=str, help="path to .csv file where relevant data is located.")
+args = parser.parse_args()
+
+dir_src = "./"
+dir_dst = "./"
+fpath_ref = ""
+
+if args.src: dir_src = args.src
+if args.dst: dir_dst = args.dst
+if args.ref: fpath_ref = args.ref
+
+# Check on which system the programme is running
+print(sys.platform)
+
+df = pd.read_csv(fpath_ref, delimiter=',', encoding="utf-8-sig")
+
+def check_dir(fpath):
+    if not os.path.isdir(fpath.rsplit("/", 1)[0]): os.mkdir(fpath.rsplit("/", 1)[0])
+
+for i in range(0,len(df)):
+    try:
+        #
+        fpath_src = os.path.join(dir_src, str(df["PID"][i]) + ".dcm")
+        if type(df["TYPE2"][i]) in [int, str]: fpath_dst = os.path.join(dir_dst, "{}/{}.dcm".format(str(df["TYPE2"][i]), df["PID"][i]))
+        else: fpath_dst = os.path.join(dir_dst, "_{}/{}.dcm".format(str(df["TYPE1"][i][0]), df["PID"][i]))
+        check_dir(fpath_dst)
+        shutil.copy(fpath_src, fpath_dst)
+        print(i, df["PID"][i], df["TYPE1"][i][0], df["TYPE2"][i], type(df["TYPE2"][i]) in [int, str])
+    except FileNotFoundError:
+        print("FileNotFoundError ad {}".format(str(df["PID"][i])))
