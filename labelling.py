5ad2524eb3015acd7bdefdf66d690774444974e2
sanghunKang <sanghunkang.dev@gmail.com>
2017-09-11 11:32:56 +0900

    to check exception

--- labelling.py
+++ labelling.py
@@ -36,6 +36,7 @@ for i in range(len(df)):
         else:
             fname = "{0}_{1}{2}_{3}.bmp".format("X", str(df["TYPE1"][i][0]), str(df["TYPE1"][i][2]), df["PID"][i])
         fpath_dst = os.path.join(dir_dst, fname)
+        print(fpath_dst)
         check_dir(fpath_dst)
         shutil.copy(fpath_src, fpath_dst)
         print(fname)
