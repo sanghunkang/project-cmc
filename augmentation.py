627a54fd07ea0ea49c0efdd89b095b14ffc87ae0
sanghunkang <sanghunkang.dev@gmail.com>
2017-09-13 16:58:02 +0900

    figuring out a proper data augmentation method2

--- augmentation.py
+++ augmentation.py
@@ -6,15 +6,16 @@ import sys, os, pickle
 from collections import Counter
 
 # Import external packages
-from scipy import ndimage
-from skimage import measure, filters
+from scipy import stats
+from skimage.color import rgb2gray, gray2rgb
+import skimage
 
 import cv2
 import PIL.Image as Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-dirpath = ".\\data_light\\pickle"
+dirpath = "..\\pickle"
 
 def read_data(fpath):
 	"""
@@ -34,88 +35,53 @@ def generate_stack_data(dirpath):
 	print("____________________________________")
 	return [read_data(os.path.join(dirpath, fpath)) for fpath in os.listdir(dirpath)]
 
-# https://stackoverflow.com/questions/22937589/how-to-add-noise-gaussian-salt-and-pepper-etc-to-image-in-python-with-opencv
-def noisy(noise_typ, image):
-	if noise_typ == "gauss":
-		row,col= image.shape
-		mean = 0
-		var = 100
-		sigma = var**0.5
-		gauss = np.random.normal(mean,sigma,(row,col))
-		gauss = gauss.reshape(row,col)
-		noisy = image + gauss
-		return noisy
-	elif noise_typ == "sp":
-		# row,col,ch = image.shape
-		s_vs_p = 0.5
-		amount = 0.01
-		out = np.copy(image)
-		# Salt mode
-		num_salt = np.ceil(amount * image.size * s_vs_p)
-		coords = [np.random.randint(0, i - 1, int(num_salt)) for i in image.shape]
-		out[coords] = 1
-
-		# Pepper mode
-		num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
-		coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
-		out[coords] = 0
-		return out
-	elif noise_typ == "poisson":
-		vals = len(np.unique(image))
-		vals = 2 ** np.ceil(np.log2(vals))
-		noisy = np.random.poisson(image * vals) / float(vals)
-		return noisy
-	elif noise_typ =="speckle":
-		row,col = image.shape
-		gauss = np.random.randn(row,col)
-		gauss = gauss.reshape(row,col)
-		noisy = image + image * gauss
-		return noisy
+def add_random_noise(img, mode, **kwargs):
+	img_modified = rgb2gray(img / 255)
+	img_modified = skimage.util.random_noise(img_modified, mode=mode, **kwargs)#'gaussian')
+	img_modified = gray2rgb(img_modified)
+	return img_modified
 
 stack_data_raw = generate_stack_data(dirpath)
 
 for data_raw in stack_data_raw:
 	print(data_raw.shape)
-	for row in data_raw[:3]:
-		arr_img = row[:-2]
-		img = arr_img.reshape((448,448,3))
+	# for row in data_raw[:3]:
+	arr_img = data_raw[0][:-3]
+	img = arr_img.reshape((448,448,3))
 
-		img_unichannel = img[:,:,0]
-		# img_pil = Image.fromarray(img)
-		# image_file = image_file.convert('L')
-		# plt.imshow(img[:,:,0])
-		# plt.show()
+	# img_sp = noisy("sp", img_unichannel)
 
-		img_gauss = noisy("gauss", img_unichannel)
-		img_sp = noisy("sp", img_unichannel)
-		img_poisson = noisy("poisson", img_unichannel)
-		img_speckle = noisy("speckle", img_unichannel)
-		# plt.imshow(img_gauss)
-		# plt.show()
+    #
+	# img_poisson = noisy("poisson", img_unichannel)
+	# img_speckle = noisy("speckle", img_unichannel)
+	# plt.imshow(img_gauss)
+	# plt.show()
 
-		# Plots
-		plt.figure(figsize=(12, 16))
+	# Plots
+	plt.figure(figsize=(12, 16))
 
-		plt.subplot(231)
-		plt.imshow(img_unichannel, cmap='gray')
-		plt.axis('off')
+	plt.subplot(231)
+	plt.imshow(add_random_noise(img, mode="gaussian", mean=0, var=0), cmap='gray')
+	plt.axis('off')
 
-		plt.subplot(232)
-		plt.imshow(img_gauss, cmap='gray')
-		plt.axis('off')
 
-		plt.subplot(233)
-		plt.imshow(img_sp, cmap='gray')
-		plt.axis('off')
+	plt.subplot(232)
+	plt.imshow(add_random_noise(img, mode="poisson"), cmap='gray')
+	plt.axis('off')
 
-		plt.subplot(235)
-		plt.imshow(img_poisson, cmap='gray')
-		plt.axis('off')
+	plt.subplot(233)
+	plt.imshow(add_random_noise(img, mode="speckle"), cmap='gray')
+	plt.axis('off')
 
-		plt.subplot(236)
-		plt.imshow(img_speckle, cmap='gray')
-		plt.axis('off')
+	plt.subplot(235)
+	plt.imshow(add_random_noise(img, mode="gaussian", var=0.01), cmap='gray')
+	plt.axis('off')
 
-		plt.tight_layout()
-		plt.show()
+	plt.subplot(236)
+	# plt.imshow(add_random_noise(img, mode="localvar", local_vars=np.random.uniform(0, 0.01, (img.shape[0], img.shape[1]))), cmap='gray')
+	plt.imshow(add_random_noise(img, mode="localvar", local_vars=stats.truncnorm.rvs(0, 0.05, size=(img.shape[0], img.shape[1]))), cmap='gray')
+	plt.axis('off')
+
+	plt.tight_layout()
+	plt.show()
 
